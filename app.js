d9af4e30a30315dc803146cd515c920066ce3298
huynguyen011097 <duchuy011097@gmail.com>
2023-10-20 10:14:20 +0700

    demo-jenkins

---
+++ app.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hiện Chữ "Chạy"</title>
+</head>
+<body>
+    <p id="chay-text"></p>
+
+    <script>
+        // Chọn phần tử có id "chay-text"
+        var chayText = document.getElementById("chay-text");
+
+        // Thiết lập nội dung của phần tử thành "Chạy"
+        chayText.textContent = "Chạy";
+    </script>
+</body>
+</html>
+
